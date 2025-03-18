545450a86a88d91596fb832f394fb227c678cd23
nbinfo <niels@bjerre.net>
2025-03-18 16:31:38 +0100

    timestamps

--- fetchKult.sh
+++ fetchKult.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$timestamp fetchKult.sh"
 cd /home/nb/repos/kultunaut/Kultunaut-libs
 source /home/nb/repos/kultunaut/Kultunaut-libs/.venv/bin/activate
 /usr/bin/git pull
--- generateUI.sh
+++ generateUI.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-echo "$timestamp"
+echo "$timestamp generateUI.sh"
 cd /home/nb/repos/kultunaut/Kultunaut-libs
 source /home/nb/repos/kultunaut/Kultunaut-libs/.venv/bin/activate
 
