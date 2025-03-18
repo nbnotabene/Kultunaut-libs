ec76b8629a0221fc25b0e7d2ee02256ea5061001
nbinfo <niels@bjerre.net>
2025-03-18 16:28:34 +0100

    2025-03-18 16:28:34 commit

--- fetchKult.sh
+++ fetchKult.sh
@@ -2,6 +2,7 @@
 
 cd /home/nb/repos/kultunaut/Kultunaut-libs
 source /home/nb/repos/kultunaut/Kultunaut-libs/.venv/bin/activate
+/usr/bin/git pull
 .venv/bin/python3 fetchKult.py
 
 
--- generateUI.sh
+++ generateUI.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$timestamp"
 cd /home/nb/repos/kultunaut/Kultunaut-libs
 source /home/nb/repos/kultunaut/Kultunaut-libs/.venv/bin/activate
+
+/usr/bin/git pull
+
 .venv/bin/python3 kultunaut/lib/UI.py
 
-timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 /usr/bin/git add .
 /usr/bin/git commit -m "$timestamp commit"
 /usr/bin/git push origin main
