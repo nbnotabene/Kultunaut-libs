d45fa4074ab6cacb14d30487034424a07d22d24b
nbnotabene <niels@bjerre.net>
2025-03-30 19:09:47 +0200

    clear sw and cache

--- webroot/sw.js
+++ webroot/sw.js
@@ -1,28 +1,62 @@
+
 const CACHE_NAME = 'svanekebio-cache-v1';
 // no cache   '/','/index.html',
 
-const urlsToCache = [
-  '/css/basic.css',
-	'/css/page.css',
-	'/css/overlay.css',
-	'/css/arr.css',
-	'/css/films.css',
-	'/img/icons/web-app-manifest-192x192.png',
-	'/img/icons/web-app-manifest-512x512.png',
-	'/img/icons/apple-touch-icon.png',
-	'/img/icons/favicon.ico'
-];
-
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+window.addEventListener('load', function() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister().then(function(boolean) {
+        if (boolean) {
+          console.log("Service worker unregistered");
+          // Clear the cache
+          if ('caches' in window) {
+            caches.keys().then(function(cacheNames) {
+              cacheNames.forEach(function(cacheName) {
+                caches.delete(cacheName).then(function() {
+                  console.log(`Cache "${cacheName}" deleted.`);
+                });
+              });
+            });
+          }
+        } else {
+          console.log("Service worker unregistration failed");
+        }
+      });
+    });
+  }
 });
+
+
+
+// if ('serviceWorker' in navigator) {
+//   navigator.serviceWorker.ready.then(registration => {
+//     registration.unregister();
+//   });
+// }
+// 
+// 
+// const urlsToCache = [
+//   '/css/basic.css',
+// 	'/css/page.css',
+// 	'/css/overlay.css',
+// 	'/css/arr.css',
+// 	'/css/films.css',
+// 	'/img/icons/web-app-manifest-192x192.png',
+// 	'/img/icons/web-app-manifest-512x512.png',
+// 	'/img/icons/apple-touch-icon.png',
+// 	'/img/icons/favicon.ico'
+// ];
+// 
+// self.addEventListener('install', function(event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
 
 // self.addEventListener('fetch', function(event) {
 //   event.respondWith(
@@ -35,41 +69,41 @@ self.addEventListener('install', function(event) {
 //       })
 //   );
 // });
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // IMPORTANT: Exclude index.html from cache match, and always fetch from network.
-        if (event.request.url.endsWith('index.html')) {
-          return fetch(event.request);
-        }
-
-        return fetch(event.request).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // IMPORTANT: Clone the response.
-            // A response is a stream and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need to clone it.
-            //var responseToCache = response.clone();
-//
-            //caches.open(CACHE_NAME)
-            //  .then(function(cache) {
-            //    cache.put(event.request, responseToCache);
-            //  });
-//
-            //return response;
-          }
-        );
-      })
-    );
-});
+// 
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+// 
+//         // IMPORTANT: Exclude index.html from cache match, and always fetch from network.
+//         if (event.request.url.endsWith('index.html')) {
+//           return fetch(event.request);
+//         }
+// 
+//         return fetch(event.request).then(
+//           function(response) {
+//             // Check if we received a valid response
+//             if(!response || response.status !== 200 || response.type !== 'basic') {
+//               return response;
+//             }
+// 
+//             // IMPORTANT: Clone the response.
+//             // A response is a stream and because we want the browser to consume the response
+//             // as well as the cache consuming the response, we need to clone it.
+//             //var responseToCache = response.clone();
+// //
+//             //caches.open(CACHE_NAME)
+//             //  .then(function(cache) {
+//             //    cache.put(event.request, responseToCache);
+//             //  });
+// //
+//             //return response;
+//           }
+//         );
+//       })
+//     );
+// });
