38ec86aabd5b6bc89b60fe96a31b2d2760da85d6
nbnotabene <niels@bjerre.net>
2025-10-22 22:11:52 +0200

    2025-10-22 22:11:42 commit

--- fetchKult.py
+++ fetchKult.py
@@ -1,23 +1,22 @@
-
-
 import asyncio
-#from kultunaut.backend.kultToCache import kultToCache
-#from kultunaut.lib import jsoncache
+
+# from kultunaut.backend.kultToCache import kultToCache
+# from kultunaut.lib import jsoncache
 from kultunaut.lib.events import Events
 from kultunaut.lib.arrangements import Arrangements
-  
+
 if __name__ == "__main__":
+    # from kultunaut.lib import jsonToDB
+    # from kultunaut.lib.events import Events
 
-  #from kultunaut.lib import jsonToDB
-  #from kultunaut.lib.events import Events
-  
-  #def kultToCache():
-  #asyncio.run(jsoncache.fetch_jsoncache())
-  #asyncio.run(jsoncache.fetch_from_kult())
-  #kultToCache()
-  ev = Events()
-  asyncio.run(ev.fetch_from_kult())
-  asyncio.run(ev.cacheToDBevents())
-  ars = Arrangements()
-  asyncio.run(ars.DBEventsToArrangs(forceUpdate=False))
+    # def kultToCache():
+    # asyncio.run(jsoncache.fetch_jsoncache())
+    # asyncio.run(jsoncache.fetch_from_kult())
+    # kultToCache()
+    ev = Events()
 
+    # SKAL FJERNES
+    # asyncio.run(ev.fetch_from_kult())
+    asyncio.run(ev.cacheToDBevents())
+    ars = Arrangements()
+    asyncio.run(ars.DBEventsToArrangs(forceUpdate=False))
