7d301c5d3d46c57ead3524bd989ca32ff78ff5b4
nbnotabene <niels@bjerre.net>
2026-04-04 21:09:33 +0200

    2026-04-04 21:09:30 commit

--- kultunaut/lib/UI.py
+++ kultunaut/lib/UI.py
@@ -1,16 +1,16 @@
 from kultunaut.lib.MariaDBInterface import MariaDBInterface
 from kultunaut.lib.JinjaRenderer import JinjaRenderer
 from kultunaut.lib.PosterImage import PosterImage
-import asyncio
 import json
 import os
 import sys
 from dotenv import dotenv_values
-conf = {**dotenv_values(".env"),**dotenv_values(".env.secret")}
-#from kultunaut.lib import lib
 
-CURDIR = os.path.dirname(os.path.abspath(os.path.realpath(sys.argv[0])))    
-#print(CURDIR)
+conf = {**dotenv_values(".env"), **dotenv_values(".env.secret")}
+# from kultunaut.lib import lib
+
+CURDIR = os.path.dirname(os.path.abspath(os.path.realpath(sys.argv[0])))
+# print(CURDIR)
 # exit()
 # ROOTDIR = os.path.dirname(os.path.dirname(CURDIR))
 # ROOTDIR = f"{conf['ROOTDIR']}"    #/home/nb/repos/kultunaut/kultunaut-libs"
@@ -19,111 +19,108 @@ TEMPLATES = f"{CURDIR}/{conf['TEMPLATES']}"
 
 
 # metaclass=lib.Singleton
-class UI():
-    """
-    """
+class UI:
+    """ """
 
     def __init__(self):
         self._db = MariaDBInterface()
         # self._type=type
         self.jinja = JinjaRenderer(TEMPLATES)
         self.output_dir = WEBROOT
-        #self.getEvents()
-        
-    async def getEvents(self):    
-        self.dbEvents= await self._db.fetchDict("select * from curArrs where mstart > now()")
-        
+        # self.getEvents()
+
+    async def getEvents(self):
+        self.dbEvents = await self._db.fetchDict(
+            "select * from curArrs where mstart > now()"
+        )
+
     async def pagesFromDB(self):
         # dbPages = self.__db.select("select * from pages")
         dbPages = await self._db.fetchDict("select * from pages")
         for data in dbPages:
-            data['relativeUrl'] = '../'
+            data["relativeUrl"] = "../"
             output_file_path = f"{WEBROOT}/pages/{data['fname']}"
-            #self.__arrEvents.jinja.render_templates(
-            self.jinja.render_templates('page.html', data, output_file_path)
+            # self.__arrEvents.jinja.render_templates(
+            self.jinja.render_templates("page.html", data, output_file_path)
 
-    #async def eventsToHTML(self):
+    # async def eventsToHTML(self):
     #    eventsdb = await self._db.fetchDict("select * from pages")e
 
     async def createArrFolders(self):
         for arr in self.dbEvents:
-            if arr['AinfoNr'] == 19235727:
-                print ("JAWS")
+            if arr["AinfoNr"] == 19235727:
+                print("JAWS")
             newFolder = f"{WEBROOT}/arr/{arr['AinfoNr']}"
-            os.makedirs(newFolder,exist_ok=True)
-            kjson = json.loads(arr['ekjson'])   #json.loads(arr['Kjson'])
-            PosterImage(newFolder,kjson['BilledeUrl'])
-            
-            data = kjson        
-            data['relativeUrl'] = "../../"
-            
-            startArr=[]
-            starterA=arr['arrstarter'].split(',')
-            startformatA=arr['startformat'].split(',')
-            #arrNrA = arr['arrnums'].split(',')
-            for i,eventnr in enumerate(arr['arrnums'].split(',')):
-                #startArr.append([int(eventnr), starterA[i], startformatA[i],'00000000'])
+            os.makedirs(newFolder, exist_ok=True)
+            kjson = json.loads(arr["ekjson"])  # json.loads(arr['Kjson'])
+            PosterImage(newFolder, kjson["BilledeUrl"])
+
+            data = kjson
+            data["relativeUrl"] = "../../"
+
+            startArr = []
+            starterA = arr["arrstarter"].split(",")
+            startformatA = arr["startformat"].split(",")
+            # arrNrA = arr['arrnums'].split(',')
+            for i, eventnr in enumerate(arr["arrnums"].split(",")):
+                # startArr.append([int(eventnr), starterA[i], startformatA[i],'00000000'])
                 startArr.append([int(eventnr), starterA[i], startformatA[i]])
-            data['startdatoer'] = startArr
-            if arr['tmdb'] is None:
-                data['tmdb'] =''                
+            data["startdatoer"] = startArr
+            if arr["tmdb"] is None:
+                data["tmdb"] = ""
             else:
                 try:
-                    jsonObj = json.loads(arr['tmdb'])
-                except(json.decoder.JSONDecodeError):
-                    jsonObj = '{error: "JSONDecodeError", errormsg: "Could not decode JSON"}'               
-                data['tmdb'] = jsonObj
-                
-            
+                    jsonObj = json.loads(arr["tmdb"])
+                except json.decoder.JSONDecodeError:
+                    jsonObj = (
+                        '{error: "JSONDecodeError", errormsg: "Could not decode JSON"}'
+                    )
+                data["tmdb"] = jsonObj
+
             f = open(f"{newFolder}/arrdata.json", "w", encoding="utf_8")
             f.write(json.dumps(data, ensure_ascii=False))
             f.close()
-            
-            
-            f = open(f"{newFolder}/tmdb.json", "w", encoding="utf_8")
-            f.write(json.dumps(arr['tmdb'], ensure_ascii=False))
-            f.close()
-            
-            #output_file_path = f"{newFolder}/index.html"
-            self.jinja.render_templates('arr.html', data, f"{newFolder}/index.html")
-            print(f"Oprettet: {kjson['AinfoNr']}")
-            
-            
-            
 
+            f = open(f"{newFolder}/tmdb.json", "w", encoding="utf_8")
+            f.write(json.dumps(arr["tmdb"], ensure_ascii=False))
+            f.close()
+
+            # output_file_path = f"{newFolder}/index.html"
+            self.jinja.render_templates("arr.html", data, f"{newFolder}/index.html")
+            print(f"Oprettet: {kjson['AinfoNr']}")
 
     async def createIndex(self):
-        #data = self.get_all_events()
-        #data = self.dbEvents
-        #self.dbEvents['relativeUrl']="./"
-        #data['list'] = list(self._arrColl)
-        #data['events'] = self.get_all_events()
-        #data = jsonpickle.encode(self._events, unpicklable=False)
-        #data = self._events.values()
-        #[17386703: 2024-03-16 19:45 Den grænseløse, .....
+        # data = self.get_all_events()
+        # data = self.dbEvents
+        # self.dbEvents['relativeUrl']="./"
+        # data['list'] = list(self._arrColl)
+        # data['events'] = self.get_all_events()
+        # data = jsonpickle.encode(self._events, unpicklable=False)
+        # data = self._events.values()
+        # [17386703: 2024-03-16 19:45 Den grænseløse, .....
 
         info_message = (
-           "Har du lyst til at være støtte-medlem? "
-           "Eller vælge din favoritfilm til visning i biografen? Check <a href='/pages/medlem.html'>dette link</a>"
-           )
-        data={ 
-            'info_message': info_message,
-            'relativeUrl': './',
-            'events': list(self.dbEvents)
+            "Har du lyst til at være støtte-medlem? "
+            "Eller vælge din favoritfilm til visning i biografen? Check <a href='/pages/medlem.html'>dette link</a>"
+        )
+        data = {
+            "info_message": info_message,
+            "relativeUrl": "./",
+            "events": list(self.dbEvents),
         }
-        
+
         output_file_path = f"{self.output_dir}/index.html"
-        self.jinja.render_templates('index.html',data,output_file_path)
-
-
+        self.jinja.render_templates("index.html", data, output_file_path)
 
 
 async def main():
     myUI = UI()
-    await myUI.getEvents()
+    # await myUI.getEvents()
     await myUI.pagesFromDB()
-    await myUI.createArrFolders()
-    await myUI.createIndex()
+    # await myUI.createArrFolders()
+    # await myUI.createIndex()
 
-# if __name__ == "__main__":
-    # asyncio.run(main())
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
